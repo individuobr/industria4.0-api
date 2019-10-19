b91f8a0075ccae5f7c340efe365ba077c3afae77
Sergio Costa <sergioamcosta@gmail.com>
2019-10-18 21:36:18 -0300

    ajuste no model passageiro, adcionndo novos campos

--- src/main/java/br/edu/unicid/api/domain/Passageiro.java
+++ src/main/java/br/edu/unicid/api/domain/Passageiro.java
@@ -1,16 +1,10 @@
 package br.edu.unicid.api.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "passageiro")
@@ -20,17 +14,17 @@ public class Passageiro {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String nome;
+	private String cpf;
 	private String assento;
-	@JsonIgnore
-	@Transient
-	private List<Bagagem> bagagem;
-	
-	public Passageiro(Integer id, String nome, String assento, List<Bagagem> bagagem) {
+	private String voo;
+		
+	public Passageiro(Integer id, String nome, String cpf, String assento, String voo) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.cpf = cpf;
 		this.assento = assento;
-		this.bagagem = bagagem;
+		this.voo = voo;
 	}
 	
 	public Passageiro() {
@@ -48,24 +42,28 @@ public class Passageiro {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
 	public String getAssento() {
 		return assento;
 	}
 	public void setAssento(String assento) {
 		this.assento = assento;
 	}
-	public List<Bagagem> getBagagem() {
-		return bagagem;
+
+	public String getVoo() {
+		return voo;
 	}
-	public void setBagagem(List<Bagagem> bagagem) {
-		this.bagagem = bagagem;
-	}
-	
-	public void acrescentarBagagem(Bagagem bagagem) {
-		if(this.bagagem.isEmpty()) {
-			this.bagagem = new ArrayList<Bagagem>();
-		}
-			this.bagagem.add(bagagem);
+
+	public void setVoo(String voo) {
+		this.voo = voo;
 	}
 	
 
